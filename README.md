7e909ad403a2c76ef788a80639041871006d9749
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-03-25 07:54:40 +0800

    VIDEO: PAYLOAD

---
+++ README.md
@@ -0,0 +1,10 @@
+# houghpy (video edition) 
+> Edge detection using Hough Transform
+
+---
+
+## Possible Outcome
+![alt text](./md-assets/cover.png "Logo Title Text 1")
+
+## Documentation
+`python3 main.py <input_video:video>`
--- main.py
+++ main.py
@@ -1,45 +1,46 @@
 import cv2
 import numpy
 
-image = cv2.imread('test.jpg')
+readVideo = cv2.VideoCapture('./video3.mp4')
 
-def detect_edges(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    newimg = cv2.Canny(gray, 75, 150)
-
-    lines = cv2.HoughLinesP(newimg, 1, numpy.pi/180, 300)
-
-    for l in lines:
-        x1,x2,y1,y2 = l[0]
-
-        linedimg = cv2.line(image, (x1,x2), (y1,y2), (0,255,0), 3)
-
-        return linedimg
-    
 while True:
 
-    # Convert to gray first
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Canny the image
-    newimg = cv2.Canny(gray, 75, 150)
-
-    # HERE IS WHERE IT DETECTS LINES
-    lines = cv2.HoughLinesP(newimg, 1, numpy.pi/180, 50)
-
-    # FOR L IN LINES
-    for l in lines:
-
-        # X1,X2,Y1,Y2 IS EQUAL TO THE 4 VALUES IN L[0] BECAUSE L IS AN ARRAY WITHIN AN ARRAY: [[VAL]] AND L[0] IS: [VAL]
-        x1,x2,y1,y2 = l[0]
-
-        # DRAW THE LINES DETECTED
-        linedimg = cv2.line(image, (x1,x2), (y1,y2), (0,255,0), 3)
-
-    cv2.imshow('Edges', image)
+    ret, img = readVideo.read()
     
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    if ret:
+        # Convert to gray first
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Canny the readVideo
+        newimg = cv2.Canny(gray, 75, 150)
+
+        # HERE IS WHERE IT DETECTS LINES
+        lines = cv2.HoughLinesP(newimg, 1, numpy.pi/180, 50)
+
+        # FOR L IN LINES
+        for l in lines:
+
+            # X1,X2,Y1,Y2 IS EQUAL TO THE 4 VALUES IN L[0] BECAUSE L IS AN ARRAY WITHIN AN $
+            x1,x2,y1,y2 = l[0]
+
+            # DRAW THE LINES DETECTED
+            linedimg = cv2.line(img, (x1,x2), (y1,y2), (255,255,0), 3)
+
+        # Check if the item is in range, so it needs the source and also the lower brightness of the color and the higher brightness of the color
+        mask = cv2.inRange(img, numpy.array([255,255,0]), numpy.array([255,255,0]))
+
+        # bitwise is basically masking, first 2 args is source and the mask is the mask
+        bitwise = cv2.bitwise_and(img, img, mask=mask)
+
+
+        cv2.imshow('Edges', bitwise)
+        
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    else:
         break
 
+readVideo.release()
 cv2.destroyAllWindows()
     
